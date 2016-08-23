e3ddf32727864314d1477238c36502beecf32eb7
Reza <rezapython@gmail.com>
2016-08-23 23:28:47 +0430

    second view to display a single post

--- blog/views.py
+++ blog/views.py
@@ -7,3 +7,15 @@ def post_list(request):  # request: the template path and the variables to rende
     return render(request,
                   'blog/post/list.html',
                   {'posts', posts})
+
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, slug=post,
+                             status = 'published',
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
+    return render(request,
+                  'blog/post/detail.html',
+                  {'post': post})
+
