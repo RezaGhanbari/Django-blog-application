34547b8c5ffc19500a360f23bbd71f40bd8195b1
Reza <rezapython@gmail.com>
2016-08-29 02:54:29 +0430

    Back-end complated.

--- mysite/settings.py
+++ mysite/settings.py
@@ -18,7 +18,7 @@ ALLOWED_HOSTS = []
 # Email (SMTP SERVER)
 
 EMAIL_HOST = 'smtp.mail.yahoo.com'
-EMAIL_HOST_USER = ''
+EMAIL_HOST_USER = 'rgh1994@yahoo.com'
 EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 465
 EMAIL_USE_TLS = True
